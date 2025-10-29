609e696169de16228aea9a55034ce5cddedfde31
EgertL123 <linnasegert@gmail.com>
2025-10-29 11:05:01 +0200

    Create error handling for unit and integration tests

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ app.use(express.json());
 
 app.use("/todos", todoRoutes)
 
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+})
+
 app.get('/', (req, res) => {
-    res.send('res test')
+    res.send('express test')
 });
 
 // app.listen(3015, () => {
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -10,6 +10,7 @@ beforeEach(() => {
     req = httpMocks.createRequest();
     res = httpMocks.createResponse();
     next = null;
+    next = jest.fn();
 });
 
 describe('TodoController.createTodo', () => {
@@ -33,5 +34,12 @@ describe('TodoController.createTodo', () => {
         await TodoModel.create.mockReturnValue(newTodo);
         await TodoController.createTodo(req, res, next);
         expect(res._getJSONData()).toStrictEqual(newTodo);
+    })
+    it("should handle errors", async () => {
+        const errorMessage = { message: "Done property missing" };
+        const rejectedPromise = Promise.reject(errorMessage);
+        TodoModel.create.mockReturnValue(rejectedPromise);
+        await TodoController.createTodo(req, res, next);
+        expect(next).toHaveBeenCalledWith(errorMessage);
     })
 })
